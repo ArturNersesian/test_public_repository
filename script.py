45b60a2e7c2f08074ef2c3cc5cf1c3dda7f0aafe
Artur <109510338+ArturNersesian@users.noreply.github.com>
2022-08-14 22:37:23 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,14 @@
-#print("Hello World")
-#print(round (3.14159**2/2))
-#pi = 31.4159265
-#print ("%.4e" % (pi))
-
-
 
 d = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
 print("Список банков", "||".join(d.keys()))
 list_percent = d.values()
 max_percent = max(list_percent)
-#print(list_percent)
+
 deposit = input("Введите сумму, которую планируете положить под проценты: ")
 print("Максимальный банковский процент:", str(max_percent))
 deposit_max = float(deposit)*(1+max_percent/100)
-#print (deposit_max)
 print ("Максимальная сумма, которую вы можете заработать за год:", str(deposit_max))
 
 
-#text = input("Введите текст:")
-#unique = list(set(text))
-#print("Количество уникальных символов: ", len(unique))
 
 
